7dabfc8b06a40b755cc331556ad1944b76fdd01d
shiho198611 <shiho198611@gmail.com>
2019-08-13 03:31:13 +0800

    feat: add problem answer
    - Add answer for problem: RotateArray(easy).

--- src/main/java/easy/RotateArray.kt
+++ src/main/java/easy/RotateArray.kt
@@ -1,4 +1,14 @@
 package easy
 
 class RotateArray {
+    fun rotate(nums: IntArray, k: Int): Unit {
+        val tmpNums = Array(nums.size) {0}
+        for(i in nums.indices) {
+            tmpNums[(i+k)%nums.size] = nums[i]
+        }
+
+        for (i in nums.indices) {
+            nums[i] = tmpNums[i]
+        }
+    }
 }

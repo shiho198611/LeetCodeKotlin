51e5d01f62fe04304f1acf69757dfddabad3bfbc
David.Huang <david.huang@skylinetw.com>
2019-08-14 18:54:22 +0800

    feat: add problem answer
    - Add problem answer: Valid Anagram(easy).

---
+++ src/main/java/easy/ValidAnagram.kt
@@ -0,0 +1,36 @@
+package easy
+
+class ValidAnagram {
+
+    fun isAnagram(s: String, t: String): Boolean {
+
+        var result = true
+
+        if(s.length != t.length) {
+            return false
+        }
+        else {
+
+            val sArray = IntArray(26){0}
+            val tArray = IntArray(26){0}
+
+            for (index in s.indices) {
+                sArray[s[index].toInt() - 97] ++
+                tArray[t[index].toInt() - 97] ++
+            }
+
+            run loop@ {
+                for (index in sArray.indices) {
+                    if(sArray[index] != tArray[index]) {
+                        result = false
+                        return@loop
+                    }
+                }
+            }
+
+            return result
+        }
+
+    }
+
+}
---
+++ src/test/java/easy/ValidAnagramTest.kt
@@ -0,0 +1,46 @@
+package easy
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class ValidAnagramTest {
+
+    lateinit var validAnagram: ValidAnagram
+
+    @Before
+    fun initTest() {
+        validAnagram = ValidAnagram()
+    }
+
+    @Test
+    fun validAnagram_Test01() {
+        val s = "anagram"
+        val t = "nagaram"
+
+        val result = validAnagram.isAnagram(s, t)
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun validAnagram_Test02() {
+        val s = "rat"
+        val t = "car"
+
+        val result = validAnagram.isAnagram(s, t)
+
+        Assert.assertTrue(!result)
+    }
+
+    @Test
+    fun validAnagram_Test03() {
+        val s = "aacc"
+        val t = "ccac"
+
+        val result = validAnagram.isAnagram(s, t)
+
+        Assert.assertTrue(!result)
+    }
+
+}

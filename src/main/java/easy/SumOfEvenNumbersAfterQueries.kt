ccecffdc18887c2776ee6af228860887d27cc2ed
shiho198611 <shiho198611@gmail.com>
2019-08-20 03:21:06 +0800

    feat: add new problem
    - Add new problem: Sum of Even Numbers After Queries(easy).
    - Add new answer.

---
+++ src/main/java/easy/SumOfEvenNumbersAfterQueries.kt
@@ -0,0 +1,31 @@
+package easy
+
+class SumOfEvenNumbersAfterQueries {
+    fun sumEvenAfterQueries(A: IntArray, queries: Array<IntArray>): IntArray {
+
+        val result = IntArray(A.size)
+        var sum = A.filter { it%2 == 0 }.fold(0){acc, i -> acc+i}
+
+        queries.forEachIndexed { index, ints ->
+
+            val aIndex = ints[1]
+            val change = ints[0]
+
+            var itemSum = A[aIndex] + change
+
+            if(A[aIndex] % 2 == 0) {
+                sum -= A[aIndex]
+            }
+
+            if(itemSum % 2 == 0) {
+                sum += itemSum
+            }
+
+            A[aIndex] += change
+
+            result[index] = sum
+        }
+
+        return result
+    }
+}

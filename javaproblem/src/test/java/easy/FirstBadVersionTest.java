73e9b5f50509d305b75220465e7f949d7b2f595e
shiho198611 <shiho198611@gmail.com>
2019-08-22 00:43:16 +0800

    test(FirstBadVersionTest): add test case
    - Add test case for problem.

--- javaproblem/src/test/java/easy/FirstBadVersionTest.java
+++ javaproblem/src/test/java/easy/FirstBadVersionTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.stubbing.Answer;
 
 public class FirstBadVersionTest {
 
@@ -17,15 +18,79 @@ public class FirstBadVersionTest {
     @Test
     public void firstBadVersion_Test01() {
 
-        Mockito.when(firstBadVersion.isBadVersion(3)).thenReturn(false);
-        Mockito.when(firstBadVersion.isBadVersion(4)).thenReturn(true);
-        Mockito.when(firstBadVersion.isBadVersion(5)).thenReturn(true);
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).then(mockBadVersions(3));
 
         int result = firstBadVersion.firstBadVersion(5);
 
-        System.out.println(result);
+        Assert.assertEquals(result, 4);
+    }
+
+    @Test
+    public void firstBadVersion_Test02() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).then(mockBadVersions(3));
+
+        int result = firstBadVersion.firstBadVersion(6);
 
         Assert.assertEquals(result, 4);
+    }
+
+    @Test
+    public void firstBadVersion_Test03() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).then(mockBadVersions(1702766718));
+
+        int result = firstBadVersion.firstBadVersion(2126753390);
+
+        Assert.assertEquals(result, 1702766719);
+    }
+
+    @Test
+    public void firstBadVersion_Test04() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).then(mockBadVersions(1));
+
+        int result = firstBadVersion.firstBadVersion(2);
+
+        Assert.assertEquals(result, 2);
+    }
+
+    @Test
+    public void firstBadVersion_Test05() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).then(mockBadVersions(1));
+
+        int result = firstBadVersion.firstBadVersion(3);
+
+        Assert.assertEquals(result, 2);
+    }
+
+    @Test
+    public void firstBadVersion_Test06() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).thenReturn(true);
+
+        int result = firstBadVersion.firstBadVersion(2);
+
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void firstBadVersion_Test07() {
+
+        Mockito.when(firstBadVersion.isBadVersion(Mockito.anyInt())).thenReturn(true);
+
+        int result = firstBadVersion.firstBadVersion(1);
+
+        Assert.assertEquals(result, 1);
+    }
+
+
+    private Answer<Boolean> mockBadVersions(int badIndex) {
+        return invocation -> {
+            int index = (int)invocation.getArguments()[0];
+            return index > badIndex;
+        };
     }
 
 }

fd249376a21799edfc950e234dcf7bea00bddc3a
shiho198611 <shiho198611@gmail.com>
2019-09-03 04:10:32 +0800

    feat(RangeAddition2): add new problem
    - Add new problem and solution: RangeAddition2(easy).

---
+++ src/main/java/easy/RangeAddition2.kt
@@ -0,0 +1,42 @@
+package easy
+
+import kotlin.math.min
+
+class RangeAddition2 {
+
+    fun maxCount(m: Int, n: Int, ops: Array<IntArray>): Int {
+
+        var minA = m
+        var minB = n
+
+        for(i in ops.indices) {
+            minA = min(ops[i][0], minA)
+            minB = min(ops[i][1], minB)
+        }
+
+        return minA*minB
+    }
+
+    fun maxCountOrigin(m: Int, n: Int, ops: Array<IntArray>): Int {
+        if(ops.isEmpty()) {
+            return m*n
+        }
+
+        var minA = Int.MAX_VALUE
+        var minB = Int.MAX_VALUE
+
+        for(i in ops.indices) {
+
+            if(ops[i][0] < minA) {
+                minA = ops[i][0]
+            }
+            if(ops[i][1] < minB) {
+                minB = ops[i][1]
+            }
+
+        }
+
+        return minA*minB
+    }
+
+}

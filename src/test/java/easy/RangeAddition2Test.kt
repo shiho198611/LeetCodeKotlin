9b1e0e5be0af05e8aeffc33a08313e205071589e
shiho198611 <shiho198611@gmail.com>
2019-09-03 04:11:51 +0800

    test(RangeAddition2): add test case
    - Add test case for problem.

---
+++ src/test/java/easy/RangeAddition2Test.kt
@@ -0,0 +1,52 @@
+package easy
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class RangeAddition2Test {
+
+    lateinit var rangeAddition2: RangeAddition2
+
+    @Before
+    fun initTest() {
+        rangeAddition2 = RangeAddition2()
+    }
+
+    @Test
+    fun rangeAddition2_Test01() {
+        val m = 3
+        val n = 3
+        val ops = arrayOf(intArrayOf(2, 2), intArrayOf(3, 3))
+        val verify = 4
+
+        val result = rangeAddition2.maxCount(m, n, ops)
+
+        Assert.assertEquals(result, verify)
+    }
+
+    @Test
+    fun rangeAddition2_Test02() {
+        val m = 3
+        val n = 3
+        val ops = arrayOf(intArrayOf(2, 1), intArrayOf(1, 2), intArrayOf(3, 3))
+        val verify = 1
+
+        val result = rangeAddition2.maxCount(m, n, ops)
+
+        Assert.assertEquals(result, verify)
+    }
+
+    @Test
+    fun rangeAddition2_Test03() {
+        val m = 3
+        val n = 3
+        val ops = emptyArray<IntArray>()
+        val verify = 9
+
+        val result = rangeAddition2.maxCount(m, n, ops)
+
+        Assert.assertEquals(result, verify)
+    }
+
+}

993f0e7cc5feb94968f8110734c1af6f9f969f97
David.Huang <david.huang@skylinetw.com>
2019-09-03 19:01:54 +0800

    test(FindPivotIndex): add test case
    - Add test case for problem.

---
+++ src/test/java/easy/FindPivotIndexTest.kt
@@ -0,0 +1,55 @@
+package easy
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class FindPivotIndexTest {
+
+    lateinit var findPivotIndex: FindPivotIndex
+
+    @Before
+    fun initTest() {
+        findPivotIndex = FindPivotIndex()
+    }
+
+    @Test
+    fun findPivotIndex_Test01() {
+        val nums = intArrayOf(1, 7, 3, 6, 5, 6)
+        val verify = 3
+
+        val result = findPivotIndex.pivotIndex(nums)
+
+        Assert.assertEquals(result, verify)
+    }
+
+    @Test
+    fun findPivotIndex_Test02() {
+        val nums = intArrayOf(1, 2, 3)
+        val verify = -1
+
+        val result = findPivotIndex.pivotIndex(nums)
+
+        Assert.assertEquals(result, verify)
+    }
+
+    @Test
+    fun findPivotIndex_Test03() {
+        val nums = intArrayOf()
+        val verify = -1
+
+        val result = findPivotIndex.pivotIndex(nums)
+
+        Assert.assertEquals(result, verify)
+    }
+
+    @Test
+    fun findPivotIndex_Test04() {
+        val nums = intArrayOf(-1,-1,-1,-1,-1,0)
+        val verify = 2
+
+        val result = findPivotIndex.pivotIndex(nums)
+
+        Assert.assertEquals(result, verify)
+    }
+}

2e6ad14a3d97ef84898eac929b145da69b70ab90
shiho198611 <shiho198611@gmail.com>
2019-09-09 23:55:40 +0800

    test(HappyNumber): add test case
    - Add test case for problem.

---
+++ src/test/java/easy/HappyNumberTest.kt
@@ -0,0 +1,47 @@
+package easy
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class HappyNumberTest {
+
+    lateinit var happyNumber: HappyNumber
+
+    @Before
+    fun initTest() {
+        happyNumber = HappyNumber()
+    }
+
+    @Test
+    fun happyNumber_Test01() {
+        val num = 19
+        val result = happyNumber.isHappy(num)
+
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun happyNumber_Test02() {
+        val num = 18
+        val result = happyNumber.isHappy(num)
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun happyNumber_Test03() {
+        val num = 156
+        val result = happyNumber.isHappy(num)
+
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun happyNumber_Test04() {
+        val num = 0
+        val result = happyNumber.isHappy(num)
+
+        Assert.assertFalse(result)
+    }
+}

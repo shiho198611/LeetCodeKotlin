d716a8c5165d82abae55d8454d9181f0936b1186
David.Huang <david.huang@skylinetw.com>
2019-09-26 13:45:10 +0800

    test(TwoSum): add test case
    - Add test case for problem.

---
+++ src/test/java/easy/TwoSumTest.kt
@@ -0,0 +1,27 @@
+package easy
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class TwoSumTest {
+
+    lateinit var twoSum: TwoSum
+
+    @Before
+    fun initTest() {
+        twoSum = TwoSum()
+    }
+
+    @Test
+    fun twoSum_Test01() {
+        val nums = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        val verify = intArrayOf(0, 1)
+
+        val result = twoSum.twoSum(nums, target)
+
+        Assert.assertArrayEquals(result, verify)
+    }
+
+}

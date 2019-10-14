a8959dd29c2cbdf79384d4c2a0d9436b8c744ee1
shiho198611 <shiho198611@gmail.com>
2019-10-14 23:05:06 +0800

    feat(DayOfTheYear): add new problem
    - Add new problem and answer.

---
+++ src/main/java/easy/DayOfTheYear.kt
@@ -0,0 +1,22 @@
+package easy
+
+class DayOfTheYear {
+
+    fun dayOfYear(date: String): Int {
+
+        val months = intArrayOf(31, 28, 31, 30, 31 ,30, 31, 31, 30, 31, 30, 31)
+        val dates = date.split("-").map { it.toInt() }
+
+        if(dates[0] % 4 == 0 && dates[0] % 100 != 0 || dates[0] % 400 == 0) {
+            months[1] += 1
+        }
+
+        var result = dates[2]
+        for(index in 0 until (dates[1] - 1) ) {
+            result += months[index]
+        }
+
+        return result
+    }
+
+}

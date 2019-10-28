4ee3242bda5d8e2350b999cd25962c838cba45f8
David.Huang <david.huang@skylinetw.com>
2019-10-28 19:06:05 +0800

    feat(JewelsAndStones): add new problem
    - Add new problem and answer.

---
+++ src/main/java/easy/JewelsAndStones.kt
@@ -0,0 +1,11 @@
+package easy
+
+class JewelsAndStones {
+    fun numJewelsInStones(J: String, S: String): Int {
+        val map = Array(123){false}
+        for(it in J) {
+            map[it.toInt()] = true
+        }
+        return S.filter { map[it.toInt()] }.length
+    }
+}

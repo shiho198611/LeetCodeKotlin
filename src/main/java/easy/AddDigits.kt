4871e80624a9cea506e6122022ced370cd45f632
David.Huang <david.huang@skylinetw.com>
2019-11-07 13:58:29 +0800

    feat(AddDigits): add new problem
    - Add new problem and answer.

---
+++ src/main/java/easy/AddDigits.kt
@@ -0,0 +1,29 @@
+package easy
+
+class AddDigits {
+    fun addDigits(num: Int): Int {
+
+        if(num < 10) {
+            return num
+        }
+
+        var digit = num
+        var result = 0
+
+        while (digit / 10 > 0) {
+            result = 0
+            while (digit != 0) {
+                result += digit % 10
+                digit /= 10
+            }
+            digit = result
+        }
+
+        return result
+    }
+
+    fun addDigitsFaster(num: Int): Int {
+        return (num - 1) % 9 + 1
+    }
+
+}
